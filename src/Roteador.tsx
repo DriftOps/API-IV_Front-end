39ede7b4822bf221ab6ba77abcf8401baade5ed5
Kalil Pereira <kalil.pereira@fatec.sp.gov.br>
2024-09-17 19:34:43 -0300

    feat. Pequena correção no local do roteador

--- src/Roteador.tsx
+++ src/Roteador.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './Login';
-import Processos from './Processos';
-import Header from './Header'; // Importando o Header
+import Login from './componentes/Login';
+import Processos from './componentes/Processos';
+import Header from './componentes/Header'; 
+import Sidebar from './componentes/sidebar/sidebar';
 
 const Roteador: React.FC = () => {
   return (
     <Router>
-      <Header /> {/* Certifique-se de que o Header é exibido em todas as páginas */}
+      <Header /> {/* Certifique-se de que o Header é exibido em todas as páginas */}    
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Roteador from './componentes/Roteador';
+import Roteador from './Roteador';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
