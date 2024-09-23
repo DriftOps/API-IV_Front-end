5d5c93035851ea26668387945a8a5d811c9c8493
Pablo Rafael <pablo7r@outlook.com>
2024-09-23 16:06:35 -0300

    adição da pasta 'img' e criação da tela de perfil, mas ainda não configurei o router

---
+++ src/componentes/Perfil.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Profile.css';
+import userphoto from './img/user.png';
+
+
+const Profile = () => {
+  return (
+    <div className="profile">
+      <img src={userphoto} alt='perfil' className='profile-pic'/>
+      <h2 className="profile-name">Bianca Capelo</h2>
+      <p className="profile-id">ID: 123456</p>
+
+      <div className="profile-info">
+        <p><strong>Nome:</strong> Bianca Capelo</p>
+        <p><strong>CPF:</strong> 123.456.789-00</p>
+        <p><strong>RG:</strong> 12.345.678-9</p>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
