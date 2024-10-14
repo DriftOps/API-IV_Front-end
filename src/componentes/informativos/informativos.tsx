c21062014f0d7e9bc4110f98fcddc6ddc2e1dff6
Felipe Pereira <felipe.pereira64@fatec.sp.gov.br>
2024-10-14 10:12:35 -0300

    feat: Att Screen "Informativos"

--- src/componentes/informativos/informativos.tsx
+++ src/componentes/informativos/informativos.tsx
@@ -4,17 +4,36 @@ import './informativos.css';
 
 const Informativo: React.FC = () => {
   const setores = [
-    { nome: 'Operacional', path: './setor/operacional' },
-    { nome: 'Recursos Humanos', path: './setor/recursos-humanos' },
-    { nome: 'Financeiro', path: './setor/financeiro' },
-    { nome: 'Almoxarifado', path: './setor/almoxarifado' },
+    {
+      nome: 'Operacional',
+      path: '/setor/operacional',
+      descricao: 'Responsável por gerenciar a produção e operações diárias.',
+    },
+    {
+      nome: 'Recursos Humanos',
+      path: '/setor/recursos-humanos',
+      descricao: 'Gerencia recrutamento, treinamento e bem-estar dos funcionários.',
+    },
+    {
+      nome: 'Financeiro',
+      path: '/setor/financeiro',
+      descricao: 'Controla o fluxo de caixa, orçamento e estratégias financeiras.',
+    },
+    {
+      nome: 'Almoxarifado',
+      path: '/setor/almoxarifado',
+      descricao: 'Gerencia o estoque e a distribuição de materiais e suprimentos.',
+    },
   ];
 
   return (
     <div className="informativo-container">
       {setores.map((setor) => (
-        <Link to={setor.path} key={setor.nome} className="setor-botao">
-          {setor.nome}
+        <Link to={setor.path} key={setor.nome} className="setor-card">
+          <div className="card-content">
+            <h3>{setor.nome}</h3>
+            <p>{setor.descricao}</p>
+          </div>
         </Link>
       ))}
     </div>
